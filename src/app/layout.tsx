853e4eed907882441208d0b520ea2a841be2c3e8
sistemas07 <sistemas07@SISTEMAS07>
2025-10-31 08:23:33 -0600

    SE AGREGO A PRODUCCION

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
+import { Metadata } from "next";
 import "../styles/globals.css";
 
-
+export const metadata: Metadata = {
+  title: "Esymbel-Clinica Médica",
+  description: "Sitio web de Esymbel Clinica Médica",
+};
 
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
